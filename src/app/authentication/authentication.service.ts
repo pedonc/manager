51023ece10e0cd0824502fbf3b59c448f611794a
pedoncadmin <45769583+pedoncadmin@users.noreply.github.com>
2020-05-03 14:09:18 -0400

    Add files via upload

---
+++ src/app/authentication/authentication.service.ts
@@ -0,0 +1,49 @@
+/*
+* Copyright (C) 2020 Curtis Glavin, Jonathan Huppi, Robert Burkey
+*
+* This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License Version 3 as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License Version 3 along with this program, under the file name LICENSE.  As of this writing, the GPL Version 3 is also posted on the web at:
+*
+* http://www.gnu.org/licenses/
+*
+* You may also obtain a copy of the license by writing to:
+*
+* Free Software Foundation, Inc.
+* 59 Temple Place, Suite 330
+* Boston, MA 02111-1307, USA
+*/
+
+import { Injectable } from '@angular/core';
+
+import { User } from '../model/user';
+import { UserService } from '../model/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticationService {
+
+  private loggedIn: User = null;
+
+  constructor(
+    private userService: UserService
+  ) { }
+
+  login(username: string, password: string): User {
+    this.loggedIn = this.userService.checkCredentials(username, password);
+    return this.loggedIn;
+  }
+
+  logout(): void {
+    this.loggedIn = null;
+  }
+
+  user(): User {
+    return this.loggedIn;
+  }
+
+}
